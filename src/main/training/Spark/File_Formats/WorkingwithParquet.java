d447e87644a69b78520c0d49c1db41be982a8acb
technoavengers <navdeep220161@gmail.com>
2023-10-11 11:17:54 +0800

    added

--- src/main/training/Spark/File_Formats/WorkingwithParquet.java
+++ src/main/training/Spark/File_Formats/WorkingwithParquet.java
@@ -1,4 +1,34 @@
-package Spark.File_Formats;
+package main.training.Spark.File_Formats;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
 
 public class WorkingwithParquet {
+
+    public static void main(String[] args) {
+
+        //Create a Spark Session to run in local mode
+        SparkSession spark = SparkSession.builder().appName("parquetApplication").master("local[*]")
+                .config("spark.testing.memory", "471859200") // Set the driver memory here
+                .getOrCreate();
+
+        String hdfsPath = "hdfs://datacouch.training.io:8020/user/training/spark-data/pageviews_by_second_par";
+
+        Dataset<Row> parquetData = spark.read().parquet(hdfsPath);
+
+        //parquetData.filter("requests > 1000").explain();
+
+        parquetData.write().mode("overwrite").json("hdfs://datacouch.training.io:8020/user/training/spark-data/output");
+
+
+        try {
+            Thread.sleep(60000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        spark.stop();
+
+
+    }
 }
